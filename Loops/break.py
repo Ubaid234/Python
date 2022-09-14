37c232b44981d145fa512c85677aae63aa0a6eef
Ubaid <mrubaid56@gmail.com>
2022-09-14 11:43:13 +0530

    break

---
+++ Loops/break.py
@@ -0,0 +1,10 @@
+times = input("How many times do I have to tell you? ")
+times = int(times)
+
+for times in range(times):
+    print("Clean up your room")
+    if times >= 3:
+        print("DO U EVEN LISTEN ANYMORE?")
+        break
+
+
