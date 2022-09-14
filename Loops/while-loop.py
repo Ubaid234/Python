5797b1955ec90241575b15f1e86abb67695f06a6
Ubaid <mrubaid56@gmail.com>
2022-09-14 11:11:23 +0530

    practice

---
+++ Loops/while-loop.py
@@ -0,0 +1,10 @@
+# msg = input("what is the password:")
+# while msg != "banana":
+#     print("WRONG")
+#     msg = input("what is the password?")
+# print("CORRECT")
+
+num = 1
+while num < 11:
+    print(num)
+    num += 1
