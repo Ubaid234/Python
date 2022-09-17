40a3230a9a29c96061d24d6fca477b189b6ae642
Ubaid <mrubaid56@gmail.com>
2022-09-17 16:03:08 +0530

    iteration over lists using while loops

---
+++ Lists/while.py
@@ -0,0 +1,8 @@
+# Iteration over lists using while loop
+
+colors = ["purple" , "green" , "blue" , "white" , "peach"]
+
+index = 0 
+while index < len(colors) :
+    print(f"{index}: {colors[index]}")
+    index += 1
