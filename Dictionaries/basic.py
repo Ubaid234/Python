90e3c20f49d21fa92765ccf7f981dd5568e44432
Ubaid <mrubaid56@gmail.com>
2022-10-15 23:30:16 +0530

    Dictionaries

---
+++ Dictionaries/basic.py
@@ -0,0 +1,25 @@
+# A Dictionary : A Data Structure that consists of key value pairs.
+
+# We use the keys to describe our data and the values to represent the data 
+
+instructor = {
+    "name": "colt",
+    "owns_dog" : True,
+    "num_courses": 4,
+    "favourite_language": "python",
+    "from" : "US",
+    "insert more" : "here"
+}
+
+print(instructor)
+
+# Keys and values are seperated by a colon 
+# Our keys are almost numbers or strings
+
+# Another Way of Creating a dictionary is using dict funtion 
+
+another_dictionary = dict(key = "value")
+print(another_dictionary)
+
+dict_second = dict(name = "sid" , section = "A" , )
+print(dict_second)
