992160a09dfbb006b30f43b7e7c027bff2b3e84b
Ubaid <mrubaid56@gmail.com>
2022-10-16 00:36:24 +0530

    Acessing all values

--- Dictionaries/access.py
+++ Dictionaries/access.py
@@ -12,4 +12,17 @@ instructor = {
 print(instructor["name"])
 print(instructor["from"])
 
+# Accessing all Values in a Dictionary
 
+for value in instructor.values():
+    print(value)
+
+# Accessing all Keys in a Dictionary
+
+for v in instructor.keys():
+    print(v)
+
+# Accessing all Values and keys in a Dictionary
+
+for data,values in instructor.items():
+    print(f"key is {data} and value is {values}")
