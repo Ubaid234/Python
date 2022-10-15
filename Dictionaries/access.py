c83139bbd014fa809d17cba0707b417be5b6d613
Ubaid <mrubaid56@gmail.com>
2022-10-15 23:41:31 +0530

    Acessing individual values

---
+++ Dictionaries/access.py
@@ -0,0 +1,15 @@
+# Accessing individual values 
+
+instructor = {
+    "name": "colt",
+    "owns_dog" : True,
+    "num_courses": 4,
+    "favourite_language": "python",
+    "from" : "US",
+    "insert more" : "here"
+}
+
+print(instructor["name"])
+print(instructor["from"])
+
+
