7b9b0a68718d621a68127c535c70c165c5f7888a
Ubaid <mrubaid56@gmail.com>
2022-10-16 22:26:11 +0530

    using in with dictionaries

---
+++ Dictionaries/check.py
@@ -0,0 +1,16 @@
+instructor = {
+    "name": "colt",
+    "owns_dog" : True,
+    "num_courses": 4,
+    "favourite_language": "python",
+    "from" : "US",
+    "insert more" : "here"
+}
+
+
+# check keys
+print("name" in instructor)
+print("phone" in instructor)
+
+# Check values
+print("colt" in instructor.values())
