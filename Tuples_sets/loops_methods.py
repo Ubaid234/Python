feca90b6b0d1f50cdd746209274cac3635f991bd
Ubaid <mrubaid56@gmail.com>
2022-10-21 00:18:54 +0530

    methods

--- Tuples_sets/loops_methods.py
+++ Tuples_sets/loops_methods.py
@@ -9,3 +9,17 @@ i = len(names) -1
 while(i >= 0):
     print(names[i])
     i -= 1
+
+# Methods with tuples 
+
+# Count
+
+x = (1,2,3,4,4,4)
+print(x.count(1))
+print(x.count(4))
+
+# index()
+
+print(x.index(1))
+print(x.index(4)) # only returns the first matching index
+
