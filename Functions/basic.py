988eb083d4a4961ed65fb29b9d795530107307f6
Ubaid <mrubaid56@gmail.com>
2022-10-22 00:15:48 +0530

    Function Structure

---
+++ Functions/basic.py
@@ -0,0 +1,21 @@
+# What is a Function
+
+# A process for executing a task 
+# It can accept input and return an output
+# Useful for executing similar procedures over and over
+
+# Why Use Functions
+
+# Stay DRY - Don't Repeat Yourself!
+# Clean up and prevent code duplication
+
+#FUNCTION STRUCTURE
+# def name_of_funtion():
+#     block of runnable code 
+
+def say_hi(): 
+    print('Hi')
+
+say_hi()
+say_hi()
+say_hi()
