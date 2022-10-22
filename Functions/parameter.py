a390911798400a90cae7e5743a69538df3c00e95
Ubaid <mrubaid56@gmail.com>
2022-10-23 00:17:36 +0530

    Default Parameters

--- Functions/parameter.py
+++ Functions/parameter.py
@@ -1,5 +1,8 @@
 # single parameter
 
+from regex import B
+
+
 def square(num):
     return num * num
 
@@ -14,3 +17,40 @@ def multiply(first , second):
 print(multiply(5,5))
 
 # Naming parameters makes them sensefull
+
+# Defualt parameters
+
+def exponent(num, power = 2):
+    return num ** power
+
+print(exponent(2,3)) #8
+print(exponent(3))
+
+# An example without default values, if not provided values will give error
+def add(a, b):
+    return a + b 
+
+# add() --> gives error
+
+# With default parameters
+def add(a = 10, b = 20):
+    return a + b 
+
+print(add())
+print(add(1,2))
+
+
+# Default Paramenters could be anything, Functions , lists , dictionaries , strings , booleans --all
+
+def add(a, b): 
+    return a + b
+
+def math(a, b , fn= add):
+    return fn(a,b)
+
+def subtract(a,b):
+    return a - b 
+
+print(math(2,2))
+
+print(math(2,2, subtract))
