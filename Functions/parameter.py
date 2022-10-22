7930a151f056b31e70f86e9c4a078277c73df3ee
Ubaid <mrubaid56@gmail.com>
2022-10-22 23:11:11 +0530

    Parameters

---
+++ Functions/parameter.py
@@ -0,0 +1,16 @@
+# single parameter
+
+def square(num):
+    return num * num
+
+print(square(4))
+print(square(8))
+
+# multi Parameters
+
+def multiply(first , second):
+    return first * second
+
+print(multiply(5,5))
+
+# Naming parameters makes them sensefull
