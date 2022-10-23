7b47222fac97e1ce82cd4804ca4f49d9738fe25c
Ubaid <mrubaid56@gmail.com>
2022-10-23 22:01:51 +0530

    Keyword Argument

--- Functions/parameter.py
+++ Functions/parameter.py
@@ -54,3 +54,12 @@ def subtract(a,b):
 print(math(2,2))
 
 print(math(2,2, subtract))
+
+
+# Keyword Arguments
+
+def full_name(first, last):
+    return f"Your name is {first} {last}"
+
+print(full_name(first= 'Colt' , last= 'Steele'))
+print(full_name(last= 'Steele' , first= 'Colt'))
