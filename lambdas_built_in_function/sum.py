5d9b4d68a426c1ba264c580b9dcd8e7583e4ce3e
Ubaid <mrubaid56@gmail.com>
2022-10-27 23:58:47 +0530

    sum ()

---
+++ lambdas_built_in_function/sum.py
@@ -0,0 +1,9 @@
+# sum 
+
+# Takes an iterable and an optional start.
+# Returns the sum of start and the items of an iterable from left to right and returns the total.
+# start defaults to 0
+
+print(sum([1,2,3,4])) # 10
+
+print(sum([1,2,3,4], -10)) # 0
