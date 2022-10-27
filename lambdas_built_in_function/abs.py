e1b9161d3e41e295fa8b5c69ab6338918bfa211e
Ubaid <mrubaid56@gmail.com>
2022-10-27 23:42:58 +0530

    abs()

---
+++ lambdas_built_in_function/abs.py
@@ -0,0 +1,13 @@
+import math
+
+# absolute value (abs)
+
+print(abs(-5))
+print(abs(5))
+print(abs(3.44444))
+print(abs(-3.44444))
+
+# inserting float with abs
+
+print(abs(float('20')))
+print(math.fabs(-4))
