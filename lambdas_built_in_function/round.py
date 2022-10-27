deb00d6b5e5b819a120224d210abcfb25606e3de
Ubaid <mrubaid56@gmail.com>
2022-10-28 00:05:41 +0530

    round()

---
+++ lambdas_built_in_function/round.py
@@ -0,0 +1,6 @@
+# round 
+
+# Return number rounded to ndigits precision after the decimal point. If ndigits is omitted or is None, it returns the nearest integer to its input.
+
+round(10.2) # 10
+round(1.212121, 2) # 1.21
