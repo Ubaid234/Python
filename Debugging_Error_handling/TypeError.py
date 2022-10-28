85a0320c21faf3dee04ca01125884c72e191c791
Ubaid <mrubaid56@gmail.com>
2022-10-29 00:02:07 +0530

    TYPE ERROR

---
+++ Debugging_Error_handling/TypeError.py
@@ -0,0 +1,12 @@
+# TypeError 
+
+# Occurs when: 
+
+# An operation or function is applied to the wrong type 
+# Python cannot interpret an operation on two data types
+
+len(5)
+# NameError: name 'test' is not defined
+
+"awesome" + []
+# TypeError : cannot concatenate 'str' and 'list' objects 
