0aedb35516c9ecb892d8c78302823c5edbcf21a8
Ubaid <mrubaid56@gmail.com>
2022-10-29 00:11:36 +0530

    ValueError

---
+++ Debugging_Error_handling/ValueError.py
@@ -0,0 +1,7 @@
+# ValueError 
+
+# This occurs when a built-in operation or function recieves an argument that has the right type but an inappropriate value
+
+int("foo")
+
+# ValueError: invalid literal for int() with base 10: 'foo'
