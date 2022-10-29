eb6f8885d703284859f5f638401bed50395aca37
Ubaid <mrubaid56@gmail.com>
2022-10-29 23:11:38 +0530

    custom modules

---
+++ Modules_/custom_modules/banana.py
@@ -0,0 +1,8 @@
+def peel():
+    return "Here is a delicious banana"
+
+def dip_in_chocolate():
+    return "Here is a delicious banana, dipped in chocolate"
+
+def sell():
+    return "There is always money in the banana stand"
---
+++ Modules_/custom_modules/fruits.py
@@ -0,0 +1,9 @@
+import banana
+import apples
+
+print(banana.dip_in_chocolate())
+print(apples.offer())
+
+from banana import dip_in_chocolate as dip
+
+print(dip())
