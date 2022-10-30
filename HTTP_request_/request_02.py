b6e46cbb7720be0cc63e30e9aa13d2740cca2e21
Ubaid <mrubaid56@gmail.com>
2022-10-31 00:31:44 +0530

    requesting json with python

---
+++ HTTP_request_/request_02.py
@@ -0,0 +1,8 @@
+from urllib import response
+import requests
+url = "https://icanhazdadjoke.com/"
+
+response = requests.get(url, headers={"Accept": "application/json"})
+# print(response.text)
+data = response.json()
+print(data["joke"])
