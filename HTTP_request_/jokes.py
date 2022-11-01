a1f4fce5693cce190cf9674c341318d405ccc5e9
Ubaid <mrubaid56@gmail.com>
2022-11-01 22:34:55 +0530

    left it incomplete , will do tommorrow , other reasons

---
+++ HTTP_request_/jokes.py
@@ -0,0 +1,19 @@
+# API PROJECT 
+
+import requests
+usr_input = input ("what would you like to search for ? ")
+url = "https://icanhazdadjoke.com/search"
+res = requests.get(
+    url,
+    headers={"Accept": "application/json"},
+    params={"term": usr_input} 
+).json()
+
+num_jokes = res["total_jokes"]
+if num_jokes > 1 : 
+    print("There are many jokes.")
+elif num_jokes == 1:
+    print("There is one joke")
+else:
+    print("There are no jokes")
+
