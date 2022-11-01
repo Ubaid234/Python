21eed6724c146a52ee25f3d3c601655a333cebe2
Ubaid <mrubaid56@gmail.com>
2022-11-01 21:49:26 +0530

    sending request with params

--- HTTP_request_/params.py
+++ HTTP_request_/params.py
@@ -1,6 +1,17 @@
-from requests import request
-
-
 # Sending request with params 
 
- 
+import requests
+url = "https://icanhazdadjoke.com/search"
+
+response = requests.get(
+    url, 
+    headers={"Accept": "application/json"},
+    params={"term": "cat", "limit": 1}
+)
+# print(response.text)
+
+data = response.json()
+print(data["results"])
+
+# print(data["joke"])
+# print(f"status: {data['status']}")
