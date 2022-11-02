2920250917972db01b1628f9e72c11ebf7a2b836
Ubaid <mrubaid56@gmail.com>
2022-11-02 23:46:47 +0530

    completed

--- HTTP_request_/jokes.py
+++ HTTP_request_/jokes.py
@@ -1,6 +1,14 @@
 # API PROJECT 
 
 import requests
+from random import choice
+from pyfiglet import figlet_format
+from termcolor import colored
+
+header = figlet_format("DAD JOKE 3000")
+header = colored(header, color='magenta')
+print(header)
+
 user_input = input ("what would you like to search for ? ")
 url = "https://icanhazdadjoke.com/search"
 res = requests.get(
@@ -10,11 +18,14 @@ res = requests.get(
 ).json()
 
 num_jokes = res["total_jokes"]
+results = res["results"]
 if num_jokes > 1 : 
-    print("There are many jokes.")
+    print(f"I found {num_jokes} jokes about {user_input} . Here is  one :")
+    print(choice(results)["joke"])
 elif num_jokes == 1:
-    print(res)
-    print("There is one joke")
+    print(f"I found one joke about {user_input}")
+    print(results[0]['joke'])
 else:
     print(f"Sorry couldn't find a joke with your term: {user_input}")
+
 
