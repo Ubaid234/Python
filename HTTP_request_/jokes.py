b2dbc740d4ac76212a2e199135d38677aa588279
Ubaid <mrubaid56@gmail.com>
2022-11-02 23:24:49 +0530

    working on it

--- HTTP_request_/jokes.py
+++ HTTP_request_/jokes.py
@@ -1,19 +1,20 @@
 # API PROJECT 
 
 import requests
-usr_input = input ("what would you like to search for ? ")
+user_input = input ("what would you like to search for ? ")
 url = "https://icanhazdadjoke.com/search"
 res = requests.get(
     url,
     headers={"Accept": "application/json"},
-    params={"term": usr_input} 
+    params={"term": user_input} 
 ).json()
 
 num_jokes = res["total_jokes"]
 if num_jokes > 1 : 
     print("There are many jokes.")
 elif num_jokes == 1:
+    print(res)
     print("There is one joke")
 else:
-    print("There are no jokes")
+    print(f"Sorry couldn't find a joke with your term: {user_input}")
 
