228bbb11c4c7b493781d391287be9dbab2f81c27
Ubaid <mrubaid56@gmail.com>
2022-11-03 22:55:56 +0530

    abstraction and encapsulation

---
+++ OOP/abstraction_encapsulation.py
@@ -0,0 +1,12 @@
+# Why OOP?
+
+# With object oriented programming, the goal is to encapsulate your code into logical, hierarchical groupings using classes so that you can reason about your code at a higher level.
+
+# Encapsulation
+
+# Encapsulation - the grouping of public and private attributes and methods into a programmatic class, making abstraction possible
+
+# Abstraction
+
+# Abstraction - exposing only "relevant" data in a class interface, hiding private attributes and methods (aka the "inner workings") from users
+
