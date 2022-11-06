20810d47000aad1645810d12d0eca69633fc84da
Ubaid <mrubaid56@gmail.com>
2022-11-07 00:26:53 +0530

    clearing inheritance concepts

---
+++ OOP/inheritance_02.py
@@ -0,0 +1,27 @@
+# Tech with Tim
+
+class Dog(object):
+    def __init__(self, name , age):
+        self.name = name
+        self.age = age
+
+    def speak(self):
+        print("Hi I am ", self.name , " and I am ", self.age , "years old")
+
+    def talk(self):
+        print("Bark!")
+
+class Cat(Dog):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self.color = color
+
+    def talk(self):
+        print('Meow!')
+
+jim = Dog('Jim', 70)
+jim.talk()
+
+tim = Cat('tim', 5, 'blue')
+tim.speak()
+tim.talk()
