739b677f4c8e036d34d7a0b96547b89549f7c2ac
Ubaid <mrubaid56@gmail.com>
2022-11-08 23:09:57 +0530

    Polymorphism with different classes

---
+++ OOP/polymorphism_02.py
@@ -0,0 +1,23 @@
+# Polymorphism & Inheritance
+
+# 1. The same class method works in a similar way for different classes
+
+# Each subclass will have a different implementation of the method.
+# If the method is not implemented in the subclass, the version in the parent class is called instead.
+
+# A common implementation of this is to have a method in a base (or parent) class that is overridden by a subclass. This is called method overriding.
+
+class Animal():
+    def speak(self):
+        raise NotImplementedError("Subclass needs to implement this method")
+
+class Dog(Animal):
+    def speak(self):
+        return "woof"
+
+class Cat(Animal):
+    def speak(self):
+        return "meow"
+
+
+
