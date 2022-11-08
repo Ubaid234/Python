64e5096f034e2cece3bc402adc1bcbbb26d34510
Ubaid <mrubaid56@gmail.com>
2022-11-08 23:05:38 +0530

    Polymorphism

---
+++ OOP/polymorphism.py
@@ -0,0 +1,21 @@
+# Polymorphism 
+
+# A key principle in OOP is the idea of polymorphism - an object can take on many (poly) forms (morph).
+
+# While a formal definition of polymorphism is more difficult, here are two important practical applications:
+
+# 2. The same operation works for different kinds of objects
+
+# Cat.speak()  # meow
+# Dog.speak()  # woof
+# Human.speak()  # yo
+
+# 1. The same class method works in a similar way for different classes
+
+sample_list = [1,2,3]
+sample_tuple = (1,2,3)
+sample_string = "awesome"
+
+len(sample_list)
+len(sample_tuple)
+len(sample_string)
