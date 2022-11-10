ee1f09a2692286fb83da2063f064d8aabed2a161
Ubaid <mrubaid56@gmail.com>
2022-11-10 23:57:08 +0530

    custom for loop

---
+++ iterators_Generators/custom_loop.py
@@ -0,0 +1,20 @@
+# custom for loop 
+
+def my_for(iterable, func):
+    iterator = iter(iterable)
+    while True:
+        try:
+            thing =next(iterator)
+        except StopIteration:
+            break
+        else:
+            func(thing)
+
+
+def square(x):
+    print(x * x)
+
+# my_for("Hello") 
+# my_for([1,2,3,4], print)
+my_for("LOL" , print)
+my_for([1,2,3,4,5], square)
