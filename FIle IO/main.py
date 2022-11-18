4c657c5e69882880140798e79e2805a12599adf5
Ubaid <mrubaid56@gmail.com>
2022-11-18 22:55:52 +0530

    Reading Files ( seek and cursors )

--- FIle IO/main.py
+++ FIle IO/main.py
@@ -1,2 +1,17 @@
+# open() and read()
 f = open("text.txt")
-print(f.read())
+# print(f.read())
+
+# seek() and readline()
+f.seek(0)
+# print(f.readline())
+
+# readlines()
+f.seek(0)
+# print(f.readlines()) # Gives us a list
+
+# closing a file
+
+f.seek(0)
+print(f.close())
+print(f.closed)
