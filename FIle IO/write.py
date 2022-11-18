69c571817a9a0ba893709e4c24bf9b34c5c99038
Ubaid <mrubaid56@gmail.com>
2022-11-18 23:21:28 +0530

    writing to text files

---
+++ FIle IO/write.py
@@ -0,0 +1,14 @@
+# writing to Text files 
+
+# with open("haikyu.txt" , "w") as f:
+#     f.write("Writing files is great\n")
+#     f.write("Here's another line of text \n")
+#     f.write("Closing now, goodbye")
+
+# with open("haikyu.txt" , "w") as f:
+#     f.write("Here is one more haiku\n")
+#     f.write("what about the older one \n")
+#     f.write("let's go check it out")
+
+with open("lol.txt" , "w") as file:
+    file.write("Hahaha" * 100000)
