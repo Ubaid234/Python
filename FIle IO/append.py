b99213a55bd06446b140058cb01ca46777e2d7eb
Ubaid <mrubaid56@gmail.com>
2022-11-18 23:43:25 +0530

    appending onto the existing text

---
+++ FIle IO/append.py
@@ -0,0 +1,5 @@
+with open("haikyu.txt" , "w") as f:
+    f.write("Writing files is great\n")
+    f.write("Here's another line of text \n")
+    f.write("Closing now, goodbye \n")
+    f.write("why this is not working")
