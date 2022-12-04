3b020ad4cbc283cd2277b8f223b186d33f838df5
Ubaid <mrubaid56@gmail.com>
2022-12-05 00:31:10 +0530

    syntax enlightenment

---
+++ Regular_expressions/info.py
@@ -0,0 +1,22 @@
+# some basic regex 
+
+
+# SOME REGEX SYNTAX
+# SOME CHARACTERS
+# \d 	digit 0-9
+# \w 	letter, digit, or underscore
+# \s 	whitespace character
+# \D 	not a digit
+# \W 	not a word character
+# \S 	not a whitespace character
+# . 	any character except line break
+
+
+# SOME QUANTIFIERS
+# + 	One or more 
+# {3} 	Exactly x times.  {3} - 3 times
+# {3,5} 	Three to five times
+# {4,} 	Four or more times
+# * 	Zero or more times
+# ? 	Once or none (optional)
+
