9258267d39f81499b3caf43ba70ee6f10d897e63
Ubaid <mrubaid56@gmail.com>
2022-12-04 23:40:59 +0530

    REGEX OR REGULAR EXPRESSIONS

---
+++ Regular_expressions/main.py
@@ -0,0 +1,19 @@
+# What are regular Expressions?
+
+# A way of describing patterns within search strings
+
+# (^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)
+
+
+# Potential Use Cases
+
+# Credit Card number validating    Phone number validating
+# Advanced find/replace in text
+# Formatting text/output
+# Syntax highlighting
+
+def hello_world(thingy):
+    #I do nothing
+    print("Hello World!")
+
+
